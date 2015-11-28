d32e4dddd42769196c40da01df1882b37b027d39
ayush9pandey <ayush.9.pandey@gmail.com>
2015-11-28 17:38:13 +0530

    The project has been completed, the video is on my youtube channel. The final report has been completed as well and is being committed now. The filtering code, though is not perfect, works ok. Some improvements are needed to make it perfect.

--- lcd_interface/simple_lcd_interface_arduino/simple_lcd_interface_arduino.ino
+++ lcd_interface/simple_lcd_interface_arduino/simple_lcd_interface_arduino.ino
@@ -2,16 +2,16 @@
 
 // initialize the library with the numbers of the interface pins
 //LiquidCrystal lcd(48,46,44,42,40,38,36,34,32,30,28);
-LiquidCrystal lcd(48,44,34,32,30,28);
+LiquidCrystal lcd(4,6,9,10,11,12);
 
 void setup() {
   // set up the LCD's number of columns and rows:
-  pinMode(52, OUTPUT);           // set pin to OUTPUT
-  digitalWrite(52, HIGH);
-  pinMode(46, OUTPUT);           // set pin to OUTPUT
-  digitalWrite(46, LOW);
-  pinMode(50, OUTPUT);           // set pin to OUTPUT
-  digitalWrite(50, LOW);// turn on pullup resistors
+  pinMode(2, OUTPUT);           // set pin to OUTPUT
+  digitalWrite(2, HIGH);
+  pinMode(5, OUTPUT);           // set pin to OUTPUT
+  digitalWrite(5, LOW);
+  pinMode(3, OUTPUT);           // set pin to OUTPUT
+  digitalWrite(3, LOW);// turn on pullup resistors
   lcd.begin(16, 2);
   // Print a message to the LCD.
   lcd.print("a");
@@ -19,10 +19,10 @@ void setup() {
   lcd.print("bb");
 
   //setting backlight
-  pinMode(24, OUTPUT);           // set pin to OUTPUT
-  digitalWrite(24, LOW);
-  pinMode(26, OUTPUT);           // set pin to OUTPUT
-  analogWrite(26, 255);
+//  pinMode(24, OUTPUT);           // set pin to OUTPUT
+//  digitalWrite(24, LOW);
+//  pinMode(26, OUTPUT);           // set pin to OUTPUT
+//  analogWrite(26, 255);
 }
 
 void loop() {
--- loadCell_nanofinal/loadCell_nanofinal.ino
+++ loadCell_nanofinal/loadCell_nanofinal.ino
@@ -7,17 +7,18 @@ int readIndex = 0;              // the index of the current reading
 int total = 0;                  // the running total
 int average = 0;                // the average
 
-int inputPin = A0;
+int inputPin = A7;
+int groundPin = A6;
 
 
 
 // initialize the library with the numbers of the interface pins
 //LiquidCrystal lcd(48,46,44,42,40,38,36,34,32,30,28);
-LiquidCrystal lcd(48,44,34,32,30,28); //using 4 data pins d4-d7. for mega
+LiquidCrystal lcd(4, 6, 9, 10, 11, 12); //using 4 data pins d4-d7. for mega
 
 
 
-//RW is gnd, RS and EN are in the func. 
+//RW is gnd, RS and EN are in the func.
 
 /*
   AnalogReadSerial
@@ -31,23 +32,23 @@ LiquidCrystal lcd(48,44,34,32,30,28); //using 4 data pins d4-d7. for mega
 void setup() {
   // initialize serial communication at 9600 bits per second:
   Serial.begin(9600);
-    // set up the LCD's number of columns and rows:
-  pinMode(52, OUTPUT);           // set pin to OUTPUT
-  digitalWrite(52, HIGH);
-  pinMode(46, OUTPUT);           // set pin RS to OUTPUT and GND
-  digitalWrite(46, LOW);
-  pinMode(50, OUTPUT);           // set pin to OUTPUT
-  digitalWrite(50, LOW);// turn on pullup resistors
+  // set up the LCD's number of columns and rows:
+  pinMode(2, OUTPUT);           // set pin to OUTPUT
+  digitalWrite(2, HIGH);
+  pinMode(5, OUTPUT);           // set pin RS to OUTPUT and GND
+  digitalWrite(5, LOW);
+  pinMode(3, OUTPUT);           // set pin to OUTPUT
+  digitalWrite(3, LOW);// turn on pullup resistors
   for (int thisReading = 0; thisReading < numReadings; thisReading++) {
     readings[thisReading] = 0;
   }
   //setting backlight
-  pinMode(24, OUTPUT);           // set pin to OUTPUT
-  digitalWrite(24, LOW);
-  pinMode(26, OUTPUT);           // set pin to OUTPUT
-  digitalWrite(26, HIGH);
- 
-  
+  //  pinMode(24, OUTPUT);           // set pin to OUTPUT
+  //  digitalWrite(24, LOW);
+  //  pinMode(26, OUTPUT);           // set pin to OUTPUT
+  //  digitalWrite(26, HIGH);
+  //
+//  analogReference(INTERNAL);
 }
 
 // the loop routine runs over and over again forever:
@@ -56,10 +57,10 @@ void loop() {
   // subtract the last reading:
   total = total - readings[readIndex];
   // read from the sensor:
-  readings[readIndex] = 1.0308*(analogRead(inputPin)-analogRead(A1)-5);
-  delay(20);
+  readings[readIndex] = 1.035445373 * (analogRead(inputPin));//-18 -analogRead(groundPin));
   // add the reading to the total:
   total = total + readings[readIndex];
+//  total = max(total, readings[readIndex]);
   // advance to the next position in the array:
   readIndex = readIndex + 1;
 
@@ -71,14 +72,16 @@ void loop() {
 
   // calculate the average:
   average = total / numReadings;
-  
+  average=average-10;
+//  average = total-13;
+
   // send it to the computer as ASCII digits
-  Serial.println(average);
- 
+  //  Serial.println(average);
+
   // Print a message to the LCD.
   lcd.begin(16, 2);
   lcd.print(average);
-  lcd.setCursor(5,0);
+  lcd.setCursor(5, 0);
   lcd.print("gm");
-  delay(200);        // delay in between reads for stability
+  delay(100);        // delay in between reads for stability
 }
